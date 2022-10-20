2fd0997188ffc87c366cc481379fba9011c020eb
Gapi505 <81484469+Gapi505@users.noreply.github.com>
2022-10-20 20:03:03 +0200

    node script init working - do not use. not complete

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ Layers = [3, 4, 2]
 Nodes = []
 
 class NODE():
-    def __init__(self, layer, bias, node_index):
+    def __init__(self, layer, node_id):
         self.layer = layer  #                                   !
-        self.bias = bias    #                                   !
-        self.node_index = node_index    #                       !   set up basic variables
+        self.bias = 0    #                                   !
+        self.node_id = node_id    #                       !   set up basic variables
         self.this_layer_node_count = Layers[self.layer]    #               !
         self.prev_layer_node_count = Layers[self.layer-1]   #   !
 
@@ -30,6 +30,11 @@ class NODE():
         if layer!=0:
             for i in range(self.prev_layer_node_count):
                self.prev_layer_node_ids.append(self.all_layers_from_start_node_count - self.prev_layer_node_count - self.this_layer_node_count + i)
+
+        self.weights = []
+        if layer!=0:
+            for i in range(len(self.prev_layer_node_ids)):
+               self.weights.append(0)
         
         self.value = 0
 
@@ -41,16 +46,18 @@ class NODE():
         
         
 
-def LAYER():
+def INIT():
     node_count = []
     for i in range(len(Layers)):
         node_count.append(Layers[i])# gets the number of neurons in this layer
+    id = 0
     for n in range(len(Layers)):
         for i in range(node_count[n]):
-            Nodes.append(NODE(n, 0, i))
+            Nodes.append(NODE(n, id))
+            id += 1
         for i in range(node_count[n]):
             Nodes[i].calculate()
-LAYER()
+INIT()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
