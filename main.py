4d32f22d46cf53f9a9a89ad4c6e4f9cc0cb39be5
Gapi505 <81484469+Gapi505@users.noreply.github.com>
2022-11-17 20:20:34 +0100

    WORKING NETWORK!!!! Yaaay!
    training script to go

--- main.py
+++ main.py
@@ -9,9 +9,23 @@ layout = [  [sg.Text('Some text on Row 1')],
 # Create the Window
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 
 Layers = [3, 4, 2]
 Nodes = []
+In = []
+Out = []
 
 class NODE():
     def __init__(self, layer, node_id):
@@ -39,15 +53,21 @@ class NODE():
         self.value = 0
         self.prev_layer_node_values = []
     def calculate(self):
+        if self.layer == 0:
+            print('idk')
         if self.layer != 0:
             for i in self.prev_layer_node_ids:
                 self.prev_layer_node_values.append(Nodes[i].value)
-        print(self.prev_layer_node_values)
-        print(self.layer)
+            for i in range(len(self.prev_layer_node_values)):
+                self.value += self.prev_layer_node_ids[i]*self.weights[i]
+            self.value += self.bias
+            print(self.prev_layer_node_values)
+            print(self.layer)
+        print(self.value)
+        if self.layer == len(Layers):
+            Out.append(self.value)
+            
 
-
-        
-        
 
 def INIT():
     node_arr = []
@@ -58,12 +78,19 @@ def INIT():
         for i in range(node_arr[layer]):
             Nodes.append(NODE(layer, id))
             id += 1
+
+def CALC_ALL():
+    if len(In) != Layers[0]:
+        print(f"{bcolors.WARNING}Error: len(In) does not match Layers[0]! Quitting!{bcolors.ENDC}")
+        exit()
     node_count = 0
     for i in range(len(Layers)):
         node_count += Layers[i]
     for i in range(node_count):
             Nodes[i].calculate()
+            
 INIT()
+CALC_ALL()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
