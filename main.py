3b9add16cf4e887a0994021b2570d86a5a1f27e3
Gapi505 <sodjagas@gmail.com>
2022-12-28 17:22:34 +0100

    gui redone - still bad added a sigmoid function bugfixes
    still to lazy to add the backpropagation algo
    im out of motivation

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import PySimpleGUI as sg
-
-sg.theme('DarkBlack')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
-# Create the Window
-window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
+import math
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
+from PyQt5.QtCore import Qt, QRect
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -20,7 +16,13 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
 
+def ModifiedSigmoid(x):
+    if x < 0:
+        x=0
+    return (1 / (1 + math.exp(-x)))-0.5
 
 Layers = [3, 4, 2]
 Nodes = []
@@ -52,21 +54,53 @@ class NODE():
         
         self.value = 0
         self.prev_layer_node_values = []
+
+
     def calculate(self):
         if self.layer == 0:
             self.value = In[self.node_id]
-        if self.layer != 0:
+        elif self.layer != 0:
             for i in self.prev_layer_node_ids:
                 self.prev_layer_node_values.append(Nodes[i].value)
             for i in range(len(self.prev_layer_node_values)):
                 self.value += self.prev_layer_node_ids[i]*self.weights[i]
             self.value += self.bias
-            print(self.prev_layer_node_values)
-            print(self.layer)
+            self.value = ModifiedSigmoid(self.value)
+            #print(self.prev_layer_node_values)
+            #print(self.layer)
         print(self.value)
         if self.layer == len(Layers):
             Out.append(self.value)
             
+def Display():
+    class Window(QMainWindow):
+        def __init__(self):
+            super().__init__()
+
+            self.setGeometry(300, 300, 1024, 720)
+            self.setWindowTitle("PyQt5 window")
+            self.show()
+            self.setStyleSheet("background-color: rgb(30,30,30);")
+        def paintEvent(self, event):
+            painter = QPainter(self)
+            brush_color = QColor(135, 206, 235)
+            pen_color = QColor(0, 0, 0)
+
+            # Create a brush and pen with the desired colors
+            brush = QBrush(brush_color)
+            pen = QPen(pen_color, 2)
+
+            # Draw a circle
+            circle_rect = QRect(50, 50, 100, 100)
+            painter.setBrush(brush)
+            painter.setPen(pen)
+            painter.drawEllipse(circle_rect)
+            self.update
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())
+    
+
 
 
 def INIT():
@@ -102,10 +136,6 @@ def Get_Node_By_Coord(layer, place_in_layer):
             
 INIT()
 CALC_ALL()
+Display()
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
-
-window.close()
+    break
